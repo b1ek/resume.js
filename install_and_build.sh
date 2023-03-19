7e7791b828f7b44298527be4941eb352830ab30e
b1ek <me@blek.codes>
2023-03-19 12:41:53 +1000

    add install and build script

---
+++ install_and_build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+yarn install
+
+yarn run build
+
+while true; do
+		read -p "Do you want to copy dist to ../../../public/static/dist? (Y/n): " yn
+		case $yn in
+				[Yy]* cp dist ../../../public/static/dist; break;;
+				[Nn]* break;;
+				[]* ) cp dist ../../../public/static/dist; break;;
+				* ) echo "(Y/n)";;
+		esac
+done
