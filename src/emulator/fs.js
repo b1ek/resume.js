c50949576b014b9fe1da5d6cf7358b200995f5ac
b1ek <me@blek.codes>
2023-03-20 21:00:05 +1000

    attempt at implementing /dev/random

--- src/emulator/fs.js
+++ src/emulator/fs.js
@@ -3,6 +3,21 @@ fs.writeFileSync('README.md', 'uwu');
 const { ufs } = require('unionfs');
 
 ufs.use(fs).use(vol.fromJSON(require('./files')));
-ufs.constants = fs.constants;
+
+if (!fs.existsSync('dev'))
+    fs.mkdirSync('dev');
+
+// /dev/random & /dev/random
+(async () => {
+    const delay = t => new Promise(resolve => setTimeout(resolve, t));;
+    const crypto = require('crypto');
+
+    while (true) {
+        await delay(3);
+        const buffer = crypto.randomBytes(16);
+        fs.writeFileSync('dev/urandom', buffer);
+        fs.writeFileSync('dev/random', buffer)
+    }
+})();
 
 module.exports = ufs;
