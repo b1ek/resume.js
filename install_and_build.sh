8b734f10edd67586442224c1375301c97b7caa4d
b1ek <me@blek.codes>
2023-03-21 23:09:23 +1000

    fix path level

--- install_and_build.sh
+++ install_and_build.sh
@@ -5,11 +5,11 @@ yarn install
 yarn run build
 
 while true; do
-		read -p "Do you want to copy dist to ../../../public/static/dist? (Y/n): " yn
+		read -p "Do you want to copy dist to ../../public/static/dist? (Y/n): " yn
 		case $yn in
-				[Yy]* cp dist ../../../public/static/dist; break;;
+				[Yy]* cp dist ../../public/static/dist; break;;
 				[Nn]* break;;
-				[]* ) cp dist ../../../public/static/dist; break;;
+				[]* ) cp dist ../../public/static/dist; break;;
 				* ) echo "(Y/n)";;
 		esac
 done
