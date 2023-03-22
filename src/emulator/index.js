8831b6931f7bae2f1f357fe608acbb66e92d73f1
b1ek <me@blek.codes>
2023-03-23 09:55:57 +1000

    add suggestion to run guide

--- src/emulator/index.js
+++ src/emulator/index.js
@@ -1,8 +1,9 @@
 module.exports = (dom) => {
     const terminal = dom.terminal;
 
-    terminal.writeln('Welcome to my online resume!')
-    terminal.writeln('Type \033[1;32mhelp\033[0m for list of commands')
+    terminal.writeln('Welcome to my online resume!');
+    terminal.writeln('Type \033[1;32mhelp\033[0m for list of commands');
+    terminal.writeln('Type \033[1;32mguide\033[0m for help');
     terminal.writeln('Use \033[1;33mAlt+C/V\033[0m to copy or paste');
     terminal.writeln('');
 
